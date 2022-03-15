c4ea585c24bf2217578e7df77e64615ae559ce29
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-16 04:51:01 +0530

    start navigation bar

---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+  render() {
+    return(
+      <nav className="navigation">
+        <a href="#">HTML</a>
+        <a href="#">CSS</a>
+        <a href="#">JavaScript</a>
+        <a href="#">React</a>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
