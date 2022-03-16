ee8585fdb37f7a52cff29a05eb94f8b1122240ce
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-16 09:31:22 +0530

    add row and columns

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ import Navigation from './components/Navigation';
 function App() {
   return (
     <React.Fragment>
-      <Navigation />
+      <div className="container-fluid">
+        <Navigation />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3" id="profile-col">
+              <h1>Profile</h1>
+            </div>
+            <div className="col-md-6" id="article-col">
+              <h1>article</h1>
+            </div>
+            <div className="col-md-3" id="article-menu-col">
+              <h1>article menu</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
