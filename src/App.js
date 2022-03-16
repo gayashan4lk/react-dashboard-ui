8b62abae15c47b9bf8aaf495952b5c9d8d70fa60
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-16 09:26:14 +0530

    add navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
       <Navigation />
-    </div>
+    </React.Fragment>
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,12 +3,43 @@ import React, { Component } from 'react';
 class Navigation extends Component {
   render() {
     return(
-      <nav className="navigation">
-        <a href="#">HTML</a>
-        <a href="#">CSS</a>
-        <a href="#">JavaScript</a>
-        <a href="#">React</a>
-      </nav>
+      <div className="navigation">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+              <a className="navbar-brand" href="#">Navbar</a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav">
+                  <li className="nav-item active">
+                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Features</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Dropdown
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><hr class="dropdown-divider" /></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Pricing</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
     );
   }
 }
